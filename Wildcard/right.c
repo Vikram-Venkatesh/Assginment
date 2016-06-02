cf8c3b492c8d097a2cf35de73e75e8c3a858e7dc
emblab <emblab@emblab-H55M-S2V>
2016-06-02 14:29:04 +0530

    Hi

---
+++ Wildcard/right.c
@@ -0,0 +1,17 @@
+/// Program to match two strings, one without wilcard characters and the other with them.
+/** @Author Vikram Venkatesh
+@Date 2-June-2016 */
+#include "func.h"
+
+int main()
+{
+	char a[] = "gt*";
+	char b[] = "gtfg";
+	printf("answer is : %d",match(a,b));
+	return 0;
+}
+
+
+
+
+
